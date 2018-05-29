e287f72ba341215ffb7dd3965dbca70755839b7b
jsonsnow <1183010734@qq.com>
2018-05-29 18:29:46 +0800

    webpack学习笔记

--- app/main.js
+++ app/main.js
@@ -1,2 +1,8 @@
-const greeter = require('./Greeter.js')
-ducument.querySelector("#root").apppendChild(greeter())
+// main.js
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import './main.css';
+
+render(<Greeter />, document.getElementById('root'));
